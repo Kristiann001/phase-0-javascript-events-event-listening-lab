7db4ae3b6e5e7d70fea93d8332f781879b11cafa
Kristiann001 <162113593+Kristiann001@users.noreply.github.com>
2024-04-04 09:34:43 +0300

    add index.js

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 function addingEventListener() {
+    const input = document.querySelector('#button');
+    input.addEventListener('click', function(){
+        alert('I was clicked!');
+    });
+  
+    function clickAlert() {
+        alert('I was clicked!');
+    }
 }
